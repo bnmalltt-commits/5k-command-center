bc0ebef16e7c7a9a895c27a59eba02bcb7601662
bnmalltt-commits <bnmalltt@gmail.com>
2026-09-15 11:49:27 +0700

    Upload drizzle/0009_secure_primary_admin.sql

---
+++ drizzle/0009_secure_primary_admin.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `members` ADD `is_primary_admin` integer DEFAULT false NOT NULL;--> statement-breakpoint
+UPDATE `members` SET `is_primary_admin` = true WHERE `id` = (SELECT `id` FROM `members` WHERE `display_name` = 'Sam' AND `role` = 'admin' ORDER BY `id` LIMIT 1);
+
